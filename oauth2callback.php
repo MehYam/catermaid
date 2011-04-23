c5f7125267c0fcdf212e186686e7af544568f322
Kira <kerbumble@yahoo.com>
2011-04-23 13:58:12 -0700

    first commit

---
+++ oauth2callback.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+<?php
+
+echo 'Thanks for signing in!'
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+    </body>
+</html>
